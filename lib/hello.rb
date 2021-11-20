dafea9b6f42370832d42bcbed9ea671c25db173e
Isaac Stephen <istephen@worcester.edu>
2021-11-20 18:36:24 -0500

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
+require 'greeter'
+
 # Default is World
-# Author: Isaac Stephen (istephen#worcester.edu)
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
